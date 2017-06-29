5276b06aeb831d68d8a1203c1083b61782098bb1
Ariran1 <arian@myprintbar.ru>
2017-06-29 18:09:33 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('./productCard/reviews/index.js');
 require('./productCard/js/productCard.js');
-require('./elements/dropList/dropList.js');
+// require('./elements/dropList/dropList.js');
 require('./elements/pigPrintsCarousel/pigPrintsCarousel.js');
 require('./advantages/index.js');
 
