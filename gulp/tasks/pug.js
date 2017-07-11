e7f5ddfbd6fde8a1a17bff227d58857ce3941931
Ariran1 <arian@myprintbar.ru>
2017-07-11 13:57:30 +0300

    Технический коммит

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   $.gulp.task('pug',function(){
-    return $.gulp.src(['./src/index.pug','./src/iconsPreview.pug'])
+    return $.gulp.src(['./src/index.pug','./src/iconsPreview.pug','./src/catalog.pug'])
       .pipe($.gp.pug({pretty: true}))
       .pipe($.gulp.dest('./build/'));
   })
